b2fcfaae80d77c9fe2e68dc81a656cf79b7e4a86
Evan Day <evanday19@gmail.com>
2025-05-05 22:28:49 -0400

    update vc alloc

--- src/vc_allocator.sv
+++ src/vc_allocator.sv
@@ -2,8 +2,10 @@
 
 module vc_allocator#(
     parameter int LANES_PER_CHANNEL = 2,
-    parameter int VC_DEPTH = 8
+    parameter int VC_DEPTH = 5,
+    parameter int FLIT_SIZE = 32
 ) (
+
 )
 
 endmodule
