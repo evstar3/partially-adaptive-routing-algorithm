686147d6ffa0abf61e09bed1b536759b9cf8fe22
Evan Day <evanday19@gmail.com>
2025-07-09 12:05:57 -0400

    multiple linear regression

--- exploration/analyze_csv.py
+++ exploration/analyze_csv.py
@@ -1,42 +1,25 @@
 #!/usr/bin/env python3
 
 import sys
-import numpy as np
-from types import SimpleNamespace
 
-import scipy
+import pandas as pd
+import statsmodels.api as sm
 
 def main():
-    names = [
-        'fault_rate',
-        'num_rows',
-        'injection_rate',
-        'average_flit_latency',
-        'average_packet_latency',
-        'flits_receivedtotal',
-        'packets_receivedtotal'
-    ]
+    data = pd.read_csv(sys.argv[1])
 
-    data = np.genfromtxt(sys.argv[1], skip_header=1, names=names, delimiter=',')
+    independent_vars = ['fault_rate', 'num_rows', 'injection_rate']
+    dependent_vars = [field for field in data.axes[1] if field not in independent_vars]
 
-    options = SimpleNamespace()
-    options.num_rows = np.unique(data['num_rows'])
-    #options.fault_rates = np.unique(data['fault_rate'])
-    options.fault_rates = np.array([i / 100 for i in range(0, 16, 1)])
-    options.injection_rates = np.unique(data['injection_rate'])
+    x = data[independent_vars]
+    x = sm.add_constant(x)
 
-    def do_regression(num_rows, fault_rate):
-        config_data = data[np.where((data[np.where(data['num_rows'] == num_rows)])['fault_rate'] == fault_rate)]
+    for dep_var in dependent_vars:
+        y = data[dep_var]
+        model = sm.OLS(y, x).fit()
+        print(model.summary())
 
-        x = config_data['injection_rate']
-        y = config_data['average_packet_latency']
 
-        slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(x, y)
-        print(f'{r_value ** 2:.6f}', slope, intercept, num_rows, fault_rate)
-
-    for num_rows in options.num_rows:
-        for fault_rate in options.fault_rates:
-            do_regression(num_rows, fault_rate)
 
 if __name__ == '__main__':
     main()
