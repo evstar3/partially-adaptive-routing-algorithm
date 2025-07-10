0b20fbb8a649256c04f316eb3cc2e984c99d9228
Evan Day <evanday19@gmail.com>
2025-07-10 12:16:29 -0400

    rcu work

---
+++ rtl/src/rcu_single.sv
@@ -0,0 +1,55 @@
+`include "src/rcu_header.sv"
+
+module rcu_single#(
+    parameter position_t THIS_POS = '{x:'0, y:'0, z:'0}
+) (
+    input  position_t  dest,
+    input  logic       up_faulty,
+    input  logic       down_faulty,
+    input  logic       rand_bit,
+
+    output port_t      outport
+);
+
+typedef enum logic [1:0] {
+    ZERO = 2'd0,
+    POS  = 2'd1,
+    NEG  = 2'd3
+} direction_t;
+
+logic [$clog2(MESH_DEPTH):0] z_hops;
+assign z_hops = {1'b0, dest.z} - {1'b0, THIS_POS.z};
+assign z_dir  = {2{z_hops[$clog2(MESH_DEPTH)]}} | {1'b0, |z_hops[$clog2(MESH_DEPTH)-1:0]};
+
+logic [$clog2(MESH_WIDTH):0] x_hops;
+assign x_hops = {1'b0, dest.X} - {1'b0, THIS_POS.x};
+assign x_dir  = {2{x_hops[$clog2(MESH_WIDTH)]}} | {1'b0, |x_hops[$clog2(MESH_WIDTH)-1:0]};
+
+logic [$clog2(MESH_HEIGHT):0] y_hops;
+assign y_hops = {1'b0, dest.y} - {1'b0, THIS_POS.y};
+assign y_dir  = {2{y_hops[$clog2(MESH_HEIGHT)]}} | {1'b0, |y_hops[$clog2(MESH_HEIGHT)-1:0]};
+
+logic x_sel;
+port_t chosen_x;
+
+always_comb begin
+
+    x_sel = x_dir == ZERO ? rand_bit : x_dir == POS;
+
+    chosen_x = x_sel ? EAST : WEST;
+
+    if (z_dir != ZERO) begin
+        if (z_dir == POS && up_faulty || z_dir == NEG && down_faulty)
+            outport = chosen_x;
+        else
+            outport = z_dir == POS ? UP : DOWN;
+    end else if (x_dir != ZERO)
+        outport = chosen_x;
+    else if (y_dir != ZERO)
+        outport = y_dir == POS ? NORTH : SOUTH;
+    else
+        outport = LOCAL;
+end
+
+
+endmodule
