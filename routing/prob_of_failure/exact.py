964e87bd4b8c898bb2a40367cb49b14a6ff15b93
Evan Day <evanday19@gmail.com>
2025-07-11 16:21:43 -0400

    added comparison to no adaptive routing

--- routing/prob_of_failure/exact.py
+++ routing/prob_of_failure/exact.py
@@ -11,7 +11,15 @@ def main():
     link_probs = [0.001, 0.005, 0.01, 0.05, 0.1, 0.15, 0.3]
     sizes = [2, 4, 8, 16]
 
-    def system_prob(link_prob, size):
+    def regular_system_prob(link_prob, size):
+        p_link_good = 1 - link_prob
+
+        p_all_links_good = mp.power(1 - link_prob, size * size)
+
+        p_system_bad = 1 - p_all_links_good
+        return p_system_bad
+
+    def my_system_prob(link_prob, size):
         p_row_bad = mp.power(link_prob, size)
 
         p_row_good = 1 - p_row_bad
@@ -21,16 +29,12 @@ def main():
         p_system_bad = 1 - p_system_good
         return p_system_bad
 
-    print(
-        '\n'.join(
-            ','.join(
-                nstr(system_prob(link_prob, size), n=3, min_fixed=-3, strip_zeros=False)
-                for link_prob in link_probs
-            )
-            for size in sizes
-        )
-    )
-    
+    for size in sizes:
+        for link_prob in link_probs:
+            reg = regular_system_prob(link_prob, size)
+            mine = my_system_prob(link_prob, size)
+            print(f'{size:.2f} {link_prob:.3f} {nstr(reg, n=6)} -> {nstr(mine, n=6)} ({nstr(reg / mine, n=3)}x)')
+        print()
 
 if __name__ == '__main__':
     main()
