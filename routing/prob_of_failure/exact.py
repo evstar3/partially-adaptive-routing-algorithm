6a5ae6a3d957229deadee978e0f5b06260c72089
Evan Day <evanday19@gmail.com>
2025-07-11 16:52:13 -0400

    route1 exact probability

--- routing/prob_of_failure/exact.py
+++ routing/prob_of_failure/exact.py
@@ -6,21 +6,21 @@ from mpmath import mp, mpf, nstr
 
 def main():
 
-    mp.dps = 50
+    mp.dps = 70
 
     link_probs = [0.001, 0.005, 0.01, 0.05, 0.1, 0.15, 0.3]
     sizes = [2, 4, 8, 16]
 
-    def regular_system_prob(link_prob, size):
-        p_link_good = 1 - link_prob
+    def regular_system_prob(p_link_bad, size):
+        p_link_good = 1 - p_link_bad
 
-        p_all_links_good = mp.power(1 - link_prob, size * size)
+        p_all_links_good = mp.power(1 - p_link_bad, size * size)
 
         p_system_bad = 1 - p_all_links_good
         return p_system_bad
 
-    def my_system_prob(link_prob, size):
-        p_row_bad = mp.power(link_prob, size)
+    def route0_system_prob(p_link_bad, size):
+        p_row_bad = mp.power(p_link_bad, size)
 
         p_row_good = 1 - p_row_bad
 
@@ -29,11 +29,32 @@ def main():
         p_system_bad = 1 - p_system_good
         return p_system_bad
 
+    def route1_system_prob(p_link_bad, size):
+        p_first_col_bad = mp.power(p_link_bad, size)
+
+        p_link_good = 1 - p_link_bad
+
+        p_row_good = mp.power(p_link_good, size - 1)
+
+        p_all_rows_good = mp.power(p_row_good, size)
+
+        p_any_row_bad = 1 - p_all_rows_good
+
+        p_system_bad = p_first_col_bad * p_any_row_bad
+        return p_system_bad
+
+
     for size in sizes:
         for link_prob in link_probs:
             reg = regular_system_prob(link_prob, size)
-            mine = my_system_prob(link_prob, size)
-            print(f'{size:.2f} {link_prob:.3f} {nstr(reg, n=6)} -> {nstr(mine, n=6)} ({nstr(reg / mine, n=3)}x)')
+            route0 = route0_system_prob(link_prob, size)
+            route1 = route1_system_prob(link_prob, size)
+            print(f'{size}x{size}, p_link_fail={link_prob:.3f}')
+            print(f'    regular routing: {nstr(reg, n=6)}')
+            print(f'            x sweep: {nstr(route0, n=6)}')
+            print(f'      x and y sweep: {nstr(route1, n=6)}')
+            print()
+
         print()
 
 if __name__ == '__main__':
