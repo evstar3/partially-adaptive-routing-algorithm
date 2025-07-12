cb7444ee30e63f8c74a8026ab2caae7afc3f3343
Evan Day <evanday19@gmail.com>
2025-07-12 18:55:02 -0400

    only single rcu needed now

--- rtl/src/rcu.sv
+++ rtl/src/rcu.sv
@@ -3,34 +3,89 @@
 module rcu#(
     parameter position_t THIS_POS = '{x:'0, y:'0, z:'0}
 ) (
-    input  logic      clk,
-    input  logic      reset,
+    input  port_t      inport,
+    input  position_t  dest,
+    input  logic       up_faulty,
+    input  logic       down_faulty,
 
-    input  position_t north_dest_in,
-    input  position_t south_dest_in,
-    input  position_t east_dest_in,
-    input  position_t west_dest_in,
-    input  position_t up_dest_in,
-    input  position_t down_dest_in,
-
-    input  logic      route_north,
-    input  logic      route_south,
-    input  logic      route_east,
-    input  logic      route_west,
-    input  logic      route_up,
-    input  logic      route_down,
-
-    input  logic      up_faulty,
-    input  logic      down_faulty,
-
-    output port_t     north_dir,
-    output port_t     south_dir,
-    output port_t     east_dir,
-    output port_t     west_dir,
-    output port_t     up_dir,
-    output port_t     down_dir
+    output port_t      outport
 );
 
+typedef enum logic [1:0] {
+    ZERO = 2'd0,
+    POS  = 2'd1,
+    NEG  = 2'd3
+} direction_t;
+
+logic [$clog2(MESH_DEPTH):0] z_hops;
+assign z_hops = {1'b0, dest.z} - {1'b0, THIS_POS.z};
+assign z_dir  = {2{z_hops[$clog2(MESH_DEPTH)]}} | {1'b0, |z_hops[$clog2(MESH_DEPTH)-1:0]};
+
+logic [$clog2(MESH_WIDTH):0] x_hops;
+assign x_hops = {1'b0, dest.X} - {1'b0, THIS_POS.x};
+assign x_dir  = {2{x_hops[$clog2(MESH_WIDTH)]}} | {1'b0, |x_hops[$clog2(MESH_WIDTH)-1:0]};
+
+logic [$clog2(MESH_HEIGHT):0] y_hops;
+assign y_hops = {1'b0, dest.y} - {1'b0, THIS_POS.y};
+assign y_dir  = {2{y_hops[$clog2(MESH_HEIGHT)]}} | {1'b0, |y_hops[$clog2(MESH_HEIGHT)-1:0]};
+
+logic x_sel;
+port_t backup;
+
+logic this_east_edge, this_west_edge, this_north_edge, this_south_edge;
+assign this_east_edge = THIS.x == MESH_WIDTH - 1;
+assign this_west_edge = THIS.x == 0;
+assign this_north_edge = THIS.y == MESH_HEIGHT - 1;
+assign this_south_edge = THIS.y == 0;
+
+always_comb begin
+    // select the backup direction
+    if (inport == EAST) begin
+        if (this_west_edge && MESH_HEIGHT == 1)
+            backup = DROP;
+        else if (this_west_edge && this_north_edge)
+            backup = SOUTH;
+        else if (this_west_edge)
+            backup = NORTH;
+        else
+            backup = WEST;
+    end else if (inport == SOUTH) begin
+        if (this_north_edge)
+            backup = SOUTH;
+        else
+            backup = NORTH;
+    end else if (inport == NORTH) begin
+        if (this_south_edge)
+            backup = DROP;
+        else
+            backup = SOUTH;
+    end else begin
+        if (MESH_WIDTH == 1 && MESH_HEIGHT == 1)
+            backup = DROP;
+        else if (this_north_edge && MESH_WIDTH == 1)
+            backup = SOUTH;
+        else if (MESH_WIDTH == 1)
+            backup = NORTH;
+        else if (this_east_edge)
+            backup = WEST;
+        else
+            backup = EAST;
+    end
+
+
+    if (z_dir != ZERO) begin
+        if (z_dir == POS && up_faulty || z_dir == NEG && down_faulty)
+            // use the random backup x direction
+            outport = backup;
+        else
+            outport = z_dir == POS ? UP : DOWN;
+    end else if (x_dir != ZERO)
+        outport = x_dir == POS ? EAST : WEST;
+    else if (y_dir != ZERO)
+        outport = y_dir == POS ? NORTH : SOUTH;
+    else
+        outport = LOCAL;
+end
 
 
 endmodule
