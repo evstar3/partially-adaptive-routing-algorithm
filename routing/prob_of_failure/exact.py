812f57cb7e1c77487f1d47012a95fa0ba6332c16
Evan Day <evanday19@gmail.com>
2025-07-12 13:57:43 -0400

    prob failure scripts updated for full x,y,z grids

--- routing/prob_of_failure/exact.py
+++ routing/prob_of_failure/exact.py
@@ -1,60 +1,30 @@
 #!/usr/bin/env python3
 
 import argparse
-from collections import defaultdict
 from mpmath import mp, mpf, nstr
 
 mp.dps = 70
 
-def regular_system_prob(p_link_bad, size):
-    p_link_good = 1 - p_link_bad
+def p_sys_fail_regular(x, y, z, pz):
+    return 1 - mp.power(1 - pz, (x * y) * (z - 1))
 
-    p_all_links_good = mp.power(1 - p_link_bad, size * size)
 
-    p_system_bad = 1 - p_all_links_good
-    return p_system_bad
-
-def route0_system_prob(p_link_bad, size):
-    p_row_bad = mp.power(p_link_bad, size)
-
-    p_row_good = 1 - p_row_bad
-
-    p_system_good = mp.power(p_row_good, size)
-
-    p_system_bad = 1 - p_system_good
-    return p_system_bad
-
-def route1_system_prob(p_link_bad, size):
-    p_first_col_bad = mp.power(p_link_bad, size)
-
-    p_row_bad_given_first_col_bad = mp.power(p_link_bad, size - 1)
-
-    p_row_good_given_first_col_bad = 1 - p_row_bad_given_first_col_bad
-
-    p_all_rows_good_given_first_col_bad = mp.power(p_row_good_given_first_col_bad, size)
-
-    p_any_row_bad_given_first_col_bad = 1 - p_all_rows_good_given_first_col_bad
-
-    p_system_bad = p_first_col_bad * p_any_row_bad_given_first_col_bad
-
-    return p_system_bad
+def p_sys_fail_adaptive(x, y, z, pz):
+    return 1 - mp.power(1 - (mp.power(pz, x) * (1 - mp.power(1 - mp.power(pz, x - 1), y))), z - 1)
 
 def main():
-    link_probs = [0.00001, 0.0001, 0.001, 0.005, 0.01, 0.05, 0.1, 0.15, 0.3]
-    sizes = [2, 4, 8, 16]
+    parser = argparse.ArgumentParser()
 
-    for size in sizes:
-        for link_prob in link_probs:
-            reg = regular_system_prob(link_prob, size)
-            route0 = route0_system_prob(link_prob, size)
-            route1 = route1_system_prob(link_prob, size)
-            print(f'{size}x{size}, p_link_fail={link_prob}')
-            print(f'    regular routing: {nstr(reg, n=6)}')
-            print(f'            x sweep: {nstr(route0, n=6)}')
-            print(f'      x and y sweep: {nstr(route1, n=6)}')
-            print()
+    parser.add_argument('x', type=int)
+    parser.add_argument('y', type=int)
+    parser.add_argument('z', type=int)
+    parser.add_argument('pz', type=mpf)
 
-        print()
+    args = parser.parse_args()
+    x, y, z, pz = args.x, args.y, args.z, args.pz
+
+    print(nstr(p_sys_fail_regular(x, y, z, pz), n=6))
+    print(nstr(p_sys_fail_adaptive(x, y, z, pz), n=6))
 
 if __name__ == '__main__':
     main()
--- routing/prob_of_failure/sim.py
+++ routing/prob_of_failure/sim.py
@@ -4,30 +4,34 @@ import argparse
 import random
 
 def main():
-
     parser = argparse.ArgumentParser()
 
-    parser.add_argument(
-        'size',
-        type=int
-    )
-    parser.add_argument(
-        'p',
-        type=float
-    )
+    parser.add_argument('x', type=int)
+    parser.add_argument('y', type=int)
+    parser.add_argument('z', type=int)
+    parser.add_argument('pz', type=float)
 
     args = parser.parse_args()
+    x, y, z, pz = args.x, args.y, args.z, args.pz
 
     runs = 1000000
-    sys_fail = 0
+    regular_count = 0
+    adaptive_count = 0
+
+
     for _ in range(runs):
-        grid = [[random.random() < args.p for col in range(args.size)] for row in range(args.size)]
+        grid = [[[random.random() < pz for col in range(y)] for row in range(x)] for layer in range(z - 1)]
 
-        if all(row[0] for row in grid):
-            if any(all(row) for row in grid):
-                sys_fail += 1
+        if any((any(row) for row in layer) for layer in grid):
+            regular_count += 1
 
-    print(sys_fail / runs)
+        for layer in grid:
+            if all(row[0] for row in layer) and any(all(row) for row in layer):
+                adaptive_count += 1
+                break
+
+    print(regular_count / runs)
+    print(adaptive_count / runs)
 
 
 if __name__ == '__main__':
